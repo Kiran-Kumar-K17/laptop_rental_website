6842f7d99da2f0c129f2ccc9aea8bc7f8b3e0523
Kiran Kumar <158074959+Kiran-Kumar-K17@users.noreply.github.com>
2026-05-07 07:05:58 -0700

    Use environment variable for front-end URL

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONT_END_URL || "http://localhost:5173",
   }),
 );
 const PORT = process.env.PORT || 8000;
